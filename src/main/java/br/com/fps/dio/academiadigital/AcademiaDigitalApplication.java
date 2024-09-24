f595812f94441855ea4aa879f7aafc9cfa30abaf
Francilene Silva <psilva.francilene@gmail.com>
2024-09-24 17:38:51 +0100

    Create and config project

---
+++ src/main/java/br/com/fps/dio/academiadigital/AcademiaDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.fps.dio.academiadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaDigitalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AcademiaDigitalApplication.class, args);
+  }
+
+}
